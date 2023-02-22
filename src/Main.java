871052ce3d845c73ce28fcf834c450453c6355a0
Emanuele Belfiori <121684864+emabelfi80@users.noreply.github.com>
2023-02-22 12:15:10 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-/**
- * Scrivere un programma che, dato un certo tempo in giorni, ore, minuti e secondi restituisca
- * il numero totale di secondi.
- * Sappiamo che un giorno ha 86400 secondi, un’ora ha 3600 secondi e un minuto 60 secondi.
- */
 
 public class Main {
     public static void main(String[] args) {
