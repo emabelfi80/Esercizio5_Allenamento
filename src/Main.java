26f563e0b23185459511bf965bc336b0e536915b
Emanuele Belfiori <121684864+emabelfi80@users.noreply.github.com>
2023-02-22 12:17:14 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,31 @@
+/**
+ * Scrivere un programma che, dato un certo tempo in giorni, ore, minuti e secondi restituisca
+ * il numero totale di secondi.
+ * Sappiamo che un giorno ha 86400 secondi, un’ora ha 3600 secondi e un minuto 60 secondi.
+ */
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Calcolo totale secondi");
 
-        //Passo i valori dal costruttore
-        Counter giorno1= new Counter(56,25,54,15);
-        //Mando i valori immessi sopra a video
-        System.out.println("Giorni: "+giorno1.giorni+" / Ore: "+giorno1.ore+ " / Minuti: "+giorno1.minuti+
-                " / Secondi: "+giorno1.secondi);
 
-        //Restituisco il valore totale dei secondi contenuti in ogni campo: gironi, ore, minuti e secondi.
-        System.out.println("Il valore totale dei secondi è: "+giorno1.totaleSecondi() + " secondi");
+        // Faccio inserire tramite scanner gli input di giorni, ore, minuti e secondi
+        Scanner input = new Scanner(System.in);
+        System.out.print("Inserisci il numero di giorni: ");
+        int giorni = input.nextInt();
+        System.out.print("Inserisci il numero di ore: ");
+        int ore = input.nextInt();
+        System.out.print("Inserisci il numero di minuti: ");
+        int minuti = input.nextInt();
+        System.out.print("Inserisci il numero di secondi: ");
+        int secondi = input.nextInt();
+
+        // Eseguo il calcolo totale
+        int totale_secondi = giorni * 86400 + ore * 3600 + minuti * 60 + secondi;
+
+        // MAndo risultato a stampa
+        System.out.println("Il tempo totale in secondi è: " + totale_secondi);
 
     }
 }
